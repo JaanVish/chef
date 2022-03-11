12997494a001434f37f8b50a080d59fbd137af09
Vishwas <vishwasmoorjani02@gmail.com>
2022-03-11 20:12:09 +0530

    courses page

---
+++ assets/header.php
@@ -0,0 +1,174 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js">
+<!--<![endif]-->
+
+<head>
+	<title>Chef IBCA – Institute of Bakery and Culinary Art</title>
+	<meta charset="utf-8">
+	<!--[if IE]>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<![endif]-->
+	<meta name="description" content="">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/custom.css">
+	<link rel="stylesheet" href="css/animations.css">
+	<link rel="stylesheet" href="css/font-awesome.css">
+	<link rel="stylesheet" href="css/main.css" class="color-switcher-link">
+	<script src="js/vendor/modernizr-2.6.2.min.js"></script>
+	
+	<!--[if lt IE 9]>
+		<script src="js/vendor/html5shiv.min.js"></script>
+		<script src="js/vendor/respond.min.js"></script>
+		<script src="js/vendor/jquery-1.12.4.min.js"></script>
+	<![endif]-->
+</head>
+
+<body>
+	<!--[if lt IE 9]>
+		<div class="bg-danger text-center">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/" class="color-main">upgrade your browser</a> to improve your experience.</div>
+	<![endif]-->
+
+
+
+	<!-- search modal -->
+	<div class="modal" tabindex="-1" role="dialog" aria-labelledby="search_modal" id="search_modal">
+		<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+			<span aria-hidden="true">&times;</span>
+		</button>
+		<div class="widget widget_search">
+			<form method="get" class="searchform search-form" action="/">
+				<div class="form-group">
+					<input type="text" value="" name="search" class="form-control" placeholder="Search keyword"
+						id="modal-search-input">
+				</div>
+				<button type="submit" class="btn">Search</button>
+			</form>
+		</div>
+	</div>
+
+	<!-- Unyson messages modal -->
+	<div class="modal fade" tabindex="-1" role="dialog" id="messages_modal">
+		<div class="fw-messages-wrap ls p-normal">
+			<!-- Uncomment this UL with LI to show messages in modal popup to your user: -->
+			<!--
+		<ul class="list-unstyled">
+			<li>Message To User</li>
+		</ul>
+		-->
+
+		</div>
+	</div>
+	<!-- eof .modal -->
+
+	<!-- wrappers for visual page editor and boxed version of template -->
+	<div id="canvas">
+		<div id="box_wrapper">
+
+			<!-- template sections -->
+
+			<div class="top-bar">
+				<div class="container p-20">
+					<div class="row">
+						<div class="col-xl-2 col-lg-2 col-11">
+							<a href="./" class="logo">
+								<img src="images/logo-chefibpa.png" alt="" width="70%">
+								<!-- <span class="logo-text color-darkgrey">
+									<strong class="color-main logo-dot"></strong> -->
+								</span>
+							</a>
+						</div>
+						<div class="col-xl-8 col-lg-8 text-center">
+							<span>
+								<a href="#" class="new-btn"><i class="fa fa-phone"></i> &nbsp;Call Now</a>
+							</span>
+							&nbsp;
+							<span>
+								<a href="#" class="new-btn"><i class="fa fa-location-arrow" aria-hidden="true"></i> &nbsp;Find Us</a>
+							</span>
+							&nbsp;
+							<span>
+								<a href="#" class="new-btn"><i class="fa fa-inr"></i> &nbsp;Pay Now</a>
+							</span>
+							&nbsp;
+							<span>
+								<a href="#" class="new-btn"><i class="fa fa-whatsapp"></i> &nbsp;Whatsapp</a>
+							</span>
+						</div>
+
+						<div class="col-xl-2 col-lg-2 text-left text-xl-right d-none d-lg-block">
+							<span>
+								<a href="#" class="btn btn-maincolor2">Visit workshop</a>
+							</span>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="header_absolute">
+				<!-- header with three Bootstrap columns - left for logo, center for navigation and right for includes-->
+				<header class="page_header s-bordertop nav-narrow ds justify-nav-center header-main">
+					<div class="container-fluid">
+						<div class="row align-items-center">
+							<div class="col-xl-2 col-lg-3 col-11" id="logo">
+								<a href="./" class="logo">
+									<img src="images/logos.png" alt="" width="70%">
+									<!-- <span class="logo-text color-darkgrey">
+										<strong class="color-main logo-dot"></strong> -->
+									</span>
+								</a>
+							</div>
+							<div class="col-xl-12 col-lg-6 col-1 text-sm-center">
+								<!-- main nav start -->
+								<nav class="top-nav">
+									<ul class="nav sf-menu">
+										<li>
+											<a href="#">Home</a>
+										</li>
+
+										<li>
+											<a href="#about">About Us</a>
+										</li>
+
+										<li>
+											<a href="#">Our Courses</a>
+										</li>
+
+										<li>
+											<a href="#">Student Corner</a>
+										</li>
+
+										<li>
+											<a href="#">Admission</a>
+										</li>
+										<li>
+											<a href="#">Visiting Faculty</a>
+										</li>
+										<li>
+											<a href="#blog">Blog</a>
+										</li>
+										<li>
+											<a href="#faq">Faq</a>
+										</li>
+										<li>
+											<a href="#contacts">Contact</a>
+										</li>
+									</ul>
+								</nav>
+								<!-- eof main nav -->
+							</div>
+						</div>
+					</div>
+					<!-- header toggler -->
+					<span class="toggle_menu">
+						<span></span>
+					</span>
+				</header>
+			</div>
---
+++ courses.php
@@ -0,0 +1,258 @@
+<?php include("assets/header.php"); ?>
+
+<section class="page_title ds p-50 pb-30">
+	<div class="container">
+		<div class="row">
+		</div>
+	</div>
+</section>
+
+<section class="page_title ds s-py-10 s-py-xl-25 breadcrumb-image s-overlay ">
+	<div class="container">
+		<div class="row">
+
+			<div class="divider-50"></div>
+
+			<div class="col-md-12 text-center">
+				<h1 class="fw-400">Programs</h1>
+				<ol class="breadcrumb">
+					<li class="breadcrumb-item">
+						<a href="./">Home</a>
+					</li>
+					<li class="breadcrumb-item">
+						<a href="#">Pages</a>
+					</li>
+					<li class="breadcrumb-item active">
+						Programs
+					</li>
+				</ol>
+			</div>
+
+			<div class="divider-50"></div>
+
+		</div>
+	</div>
+</section>
+
+<section class="ls s-pb-0 c-gutter-60 program-single">
+	<div class="container">
+		<div class="row">
+
+			<div class="d-none d-lg-block divider-60"></div>
+
+			<main class="col-lg-7 col-xl-8 vertical-item content-padding">
+				<div class="item-media">
+					<img src="images/gallery/06.jpg" alt="">
+
+					<div class="content-absolute bg-maincolor-transparent text-left ds">
+						<div class="d-inline">
+							<span>
+								<i class="fa fa-users color-light"></i>
+								24
+							</span>
+							<span>
+								<i class="fa fa-comments color-light"></i>
+								259
+							</span>
+							<span>
+								<i class="fa fa-money color-light"></i>
+								359$
+							</span>
+						</div>
+					</div>
+				</div>
+
+				<div class="item-content bordered">
+
+					<h4>18 Months Advance Diploma Professional Programmes Course</h4>
+
+					<p>This 18 months Level 3 Advanced Diploma in Food Preparation and Cookery Supervision (8065-04) Course accredited by City and Guilds, London(U.K) is ideal for anyone Looking for a successful career in food preparation and cookery, from new starters to experienced professional looking to specialize in Culinary or move into supervisor role. A combination of theoretical and practical classes, this extensive course gives a thorough insight for learners about the various techniques prevailing in the industry.
+					</p>
+
+					<h4>How the Course is Structured?</h4>
+					
+					<p>
+					This 18 Months Level 3 course program has all the elements to transform young amateurs into Culinary professionals. For 12 months students go through extensive practical training under the guidance of IBCA professors who give them a thorough learning and best industry practices. Grinding the next generation of students into basic fundamentals, techniques and daily activities that are must for their learning curve.
+					</p>
+					<p>
+					For the next 6 months, students are on live industrial training in the top most five-star hotels and restaurant for experiencing the daily work schedules for maintaining as a professional chef. After which their internal assessment is carried out with examinations later along with there are opportunities to get placements in the top most hotels and restaurant chains.
+					</p>
+
+					<div class="row c-mb-5 c-mb-lg-25">
+						<div class="col-md-6">
+							<h5>You Will Learn:</h5>
+
+							<span class="small-text fs-14">Andouille Qui</span>
+							<div class="progress">
+								<div class="progress-bar bg-maincolor" role="progressbar" data-transitiongoal="25" aria-valuemin="0" aria-valuemax="100">
+									<span>25%</span>
+								</div>
+							</div>
+
+
+							<span class="small-text fs-14">pancetta meatloaf</span>
+							<div class="progress">
+								<div class="progress-bar bg-maincolor2" role="progressbar" data-transitiongoal="50" aria-valuemin="0" aria-valuemax="100">
+									<span>50%</span>
+								</div>
+							</div>
+
+							<span class="small-text fs-14">Spare ribs ex</span>
+							<div class="progress">
+								<div class="progress-bar bg-maincolor" role="progressbar" data-transitiongoal="75" aria-valuemin="0" aria-valuemax="100">
+									<span>75%</span>
+								</div>
+							</div>
+							<span class="small-text fs-14">in occaecat fugiat</span>
+							<div class="progress">
+								<div class="progress-bar bg-maincolor2" role="progressbar" data-transitiongoal="100" aria-valuemin="0" aria-valuemax="100">
+									<span>100%</span>
+								</div>
+							</div>
+
+						</div>
+						<div class="col-md-6 professional">
+							<h5>Class Includes:</h5>
+							<ul style="list-style-type:none;">
+            	<li><i class="fa fa-angle-double-right" aria-hidden="true"></i>Chef uniform</li>
+              	<li><i class="fa fa-angle-double-right" aria-hidden="true"></i>Kitchen tools kit</li>
+              	<li><i class="fa fa-angle-double-right" aria-hidden="true"></i>City and Guilds Registration fee</li>
+              	<li><i class="fa fa-angle-double-right" aria-hidden="true"></i>Examination fee(Level 2 and Level 3)</li>
+              	<li><i class="fa fa-angle-double-right" aria-hidden="true"></i>Full course study and raw material </li>
+            
+            	<li><i class="fa fa-angle-double-right" aria-hidden="true"></i>Every day tasting session/take home your food</li>
+             	<li><i class="fa fa-angle-double-right" aria-hidden="true"></i>Internship in 5 Star hotels/Fine Dining Restaurant</li>
+               	<li><i class="fa fa-angle-double-right" aria-hidden="true"></i>100% Assistance in Placement after Course Completion</li>
+             </ul>
+						</div>
+					</div>
+					<!-- .row -->
+
+					<div class="professional">
+            <h3 class="heading1">Course Accreditation:</h3>
+            <p style="text-align:left;">This Diploma is accredited by City &amp; Guilds, London(U.K.) which upon completion can pursue a career in this artistic field or take a further initiative in International degree and Courses.</p>
+            <h3 class="heading1">Who Can Enroll?</h3>
+            <ul>
+              <li>Minimum 16 years of Age</li>
+              <li>Students must be 10th Passed from any certification board.</li>
+            </ul>
+            <h3 class="heading1">Sessions:</h3>
+            <p style="text-align:left;">Winter Session: February</p>
+            <p style="text-align:left;">Summer Session: July</p>
+            <h3 class="heading1">Timings:</h3>
+			<p style="text-align:left;">Morning Batch: 09:30 am to 01:30 pm</p>
+			<p style="text-align:left;">Evening Batch: 02:00 pm to 06:00 pm</p>
+            
+            <h3 class="heading1">Weekly Details:</h3>
+            <ul>
+           		<li><i class="fa fa-angle-double-right" aria-hidden="true"></i><strong>Week 1</strong>: Food safety hygienic and Theory, measures, Knife skill, cutting and conversions.</li>
+                <li><i class="fa fa-angle-double-right" aria-hidden="true"></i><strong>Week 2</strong>: Basic knowledge and introduction of ingredients.</li>
+              	<li><i class="fa fa-angle-double-right" aria-hidden="true"></i><strong>Week 3&amp;4</strong>: Practical Cooking techniques and cold preparations</li>
+                      <li><i class="fa fa-angle-double-right" aria-hidden="true"></i><strong>Week 5&amp;6</strong>: Basic Soups stocks and Basic sauces</li>
+                      <li><i class="fa fa-angle-double-right" aria-hidden="true"></i><strong>Week 7&amp;8</strong>: Vegetable, fruits ,pulses and potatoes preparations</li>
+                      <li><i class="fa fa-angle-double-right" aria-hidden="true"></i><strong>Week 9&amp;10</strong>: Meat, poultry and offal  preparations</li>
+                      <li><i class="fa fa-angle-double-right" aria-hidden="true"></i><strong>Week 11&amp;12</strong>: fish and shellfish preparations</li>
+<li><i class="fa fa-angle-double-right" aria-hidden="true"></i><strong>Week 13&amp;14</strong>: Rice ,grain, farinaceous  and  eggs preparations</li>
+<li><i class="fa fa-angle-double-right" aria-hidden="true"></i><strong>Week 15&amp;16</strong>:  Buffet and staff meal ,Healthier food and special diets</li>
+<li><i class="fa fa-angle-double-right" aria-hidden="true"></i><strong>Week 17&amp;18</strong>: Indian Cuisine and International Cuisine</li>
+<li><i class="fa fa-angle-double-right" aria-hidden="true"></i><strong>Week 19&amp;20</strong>: Practical culinary  techniques, French pastries</li>
+<li><i class="fa fa-angle-double-right" aria-hidden="true"></i><strong>Week 21&amp;22</strong>: Specialty cakes, hot ,cold pudding  and plated desserts</li>
+<li><i class="fa fa-angle-double-right" aria-hidden="true"></i><strong>Week 23&amp;24</strong>: Interpersonal &amp; Communication Skills Work effectively as a cook Provide service to customers Show social and cultural sensitivity Manage conflict Communicate on the telephone Coach others in job skills Exam-Theory and Practical (Mandatory for Certification) Arranged by IBCA –New Delhi</li>
+<li><i class="fa fa-angle-double-right" aria-hidden="true"></i><strong>Week 25</strong>: Supervisory skills in the hospitality industry</li>
+<li><i class="fa fa-angle-double-right" aria-hidden="true"></i><strong>Week 26</strong>: Developing productive working relationships with colleagues</li>
+<li><i class="fa fa-angle-double-right" aria-hidden="true"></i><strong>Week-27</strong>: Contributing to the control of resources</li>
+<li><i class="fa fa-angle-double-right" aria-hidden="true"></i><strong>Week 28</strong>: Contributing to the development of recipes and menus.</li>
+<li><i class="fa fa-angle-double-right" aria-hidden="true"></i><strong>Week 29</strong>: Maintain the health, hygiene, safety and security of the working environment</li>
+<li><i class="fa fa-angle-double-right" aria-hidden="true"></i><strong>Week 30</strong>: The principles of food safety supervision for catering</li>
+<li><i class="fa fa-angle-double-right" aria-hidden="true"></i><strong>Week 31&amp;32</strong>: Practical gastronomy, Menu planning and costing</li>
+<li><i class="fa fa-angle-double-right" aria-hidden="true"></i><strong>Week 33&amp;34</strong>: Canapés and fusion Cuisine.</li>
+<li><i class="fa fa-angle-double-right" aria-hidden="true"></i><strong>Week 35&amp;36</strong>: Italian and French Cuisine</li>
+<li><i class="fa fa-angle-double-right" aria-hidden="true"></i><strong>Week 37&amp;38</strong>: Complex Sauces Game and lamb preparations</li>
+<li><i class="fa fa-angle-double-right" aria-hidden="true"></i><strong>Week 39&amp;40</strong>: Produce hot, cold and frozen desserts,.</li>
+<li><i class="fa fa-angle-double-right" aria-hidden="true"></i><strong>Week 41&amp;42</strong>: Produce biscuits, cakes and sponges</li>
+<li><i class="fa fa-angle-double-right" aria-hidden="true"></i><strong>Week 43&amp;44</strong>: Produce petit fours ice cream and sorbets</li>
+<li><i class="fa fa-angle-double-right" aria-hidden="true"></i><strong>Week 45&amp;46</strong>: Vegetable  and chocolate sculpture, buffet presentation and plating.</li>
+<li><i class="fa fa-angle-double-right" aria-hidden="true"></i><strong>Week 47&amp;48</strong>: Final Assessment preparation for food trial and Job placement.</li>
+
+                      
+                      
+                      
+                      
+                      
+                    </ul>
+          </div>
+
+				</div>
+			</main>
+			<!-- .col-* -->
+			<aside class="col-lg-5 col-xl-4">
+				<!-- <div class="bg-maincolor2 widget-search p-30 mb-60">
+					<div class="widget widget_search">
+						<h5>Search On Website</h5>
+						<p>Find more exciting news and offers</p>
+						<form role="search" method="get" class="search-form" action="/">
+							<label for="search-form-widget">
+								<span class="screen-reader-text">Search for:</span>
+							</label>
+							<input type="search" id="search-form-widget" class="search-field form-control" placeholder="Type Keyword Here..." value="" name="search">
+							<button type="submit" class="search-submit">
+								<span class="screen-reader-text">Type Keyword Here...</span>
+							</button>
+						</form>
+					</div>
+				</div> -->
+
+				<div class="widget widget_categories">
+
+				<div class="professional myprofession">
+            <p style="text-align:left;"> </p>
+            <h3 class="widget-title">Fees Structure:</h3>
+            <ul>
+              <li><i class="fa fa-angle-double-right" aria-hidden="true"></i>Booking Amount-Rs.20,000</li>
+              <li><i class="fa fa-angle-double-right" aria-hidden="true"></i>Tuition Fees-Rs.4,20,000</li>
+              <li><i class="fa fa-angle-double-right" aria-hidden="true"></i>Examination Fees-Rs.80,000(Level-2 &amp; level-3)</li>
+              <li><i class="fa fa-angle-double-right" aria-hidden="true"></i>Total-Rs.5,20,000</li>
+              <li><i class="fa fa-angle-double-right" aria-hidden="true"></i>GST(18%)-Rs.93,600</li>
+              <li><i class="fa fa-angle-double-right" aria-hidden="true"></i>Grand Total-Rs.6,13,600</li>
+            </ul>
+
+						<div class="pt-20 pb-20">
+						<a href="course_pdf/18_months_culinary_course.pdf" target="_blank" tabindex="-1" download-""="">
+            <button>Download Brochure<span class="glyphicon glyphicon-menu-right gi-1x"></span></button>
+                    </a>
+						</div>            
+             </div>
+
+					<!-- <h3 class="widget-title">Our Programs</h3>
+
+					<ul>
+						<li class="with-icon">
+							<i class="color-main fa fa-cogs" aria-hidden="true"></i>
+							<a href="blog-right.html">All Months</a>/112
+						</li>
+						<li>
+							<a href="blog-right.html">July 2018</a>/21
+						</li>
+						<li>
+							<a href="blog-right.html">June 2018</a>/39
+						</li>
+						<li>
+							<a href="blog-right.html">May 2018</a>/7
+						</li>
+						<li>
+							<a href="blog-right.html">April 2018</a>/16
+						</li>
+						<li>
+							<a href="blog-right.html">March 2018</a>/25
+						</li>
+					</ul> -->
+				</div>
+			</aside>
+
+			<div class="d-none d-lg-block divider-60"></div>
+
+		</div>
+	</div>
+</section>
+
+<?php include("assets/footer.php"); ?>
